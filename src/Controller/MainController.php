3414b21f5e6903c0cf599123ad3fac757e5bab7e
Tenzin <gyurmey1835@gmail.com>
2021-01-06 22:58:35 +0100

    view twig

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -11,8 +12,23 @@ class MainController extends AbstractController
     /**
      * @Route("/", name="home")
      */
-    public function index(): Response
+    public function index()
     {
-        return new Response('<h1> Welcome PHP !</h1>');
+        // return new Response('<h1> Welcome PHP !</h1>');
+        return $this->render('home/index.html.twig');
+
+    }
+
+    /**
+     * @Route("/custom/{name?}", name="custom")
+     * @param Request $request
+     * @return Response
+     */
+    public function custom(Request $request){
+        $name = ($request->get('name'));
+        // return new Response('<h1> Welcome !</h1>');
+        return $this->render('home/custom.html.twig', [
+            'name' => $name
+        ]);
     }
 }
